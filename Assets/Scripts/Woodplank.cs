814c79ce164cda0d7ec4c0807a406f7b7d1ff480
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2022-05-20 16:20:24 -0300

    Add comments explaining the code

--- Assets/Scripts/Woodplank.cs
+++ Assets/Scripts/Woodplank.cs
@@ -1,4 +1,3 @@
-    using System;
 using UnityEngine;
 
 public class Woodplank : MonoBehaviour
@@ -10,11 +9,21 @@ public class Woodplank : MonoBehaviour
         if (!collision.gameObject.CompareTag("Ball"))
             return;
 
-        var position = transform.position + boxCollider.size.y / 2 * Vector3.up;
-        var halfExtents = Vector3.Scale(transform.lossyScale, boxCollider.size) / 2;
-        var layerMask = LayerMask.GetMask("Objects");
+        // The OverlapBox checks if there are any colliders within the target box area:
+        // https://docs.unity3d.com/ScriptReference/Physics.OverlapBox.html
 
-        var colliders = Physics.OverlapBox(
+        // We position the box check right at the edge of the plank
+        Vector3 position = transform.position + boxCollider.size.y / 2 * Vector3.up;
+
+        // We use the collider size to get the correct size of the box we are checking
+        // But we need to multiply by the scale, to transform it from local to world
+        Vector3 halfExtents = Vector3.Scale(transform.lossyScale, boxCollider.size) / 2;
+
+        // To optimize our search, we can filter the checks to a specific layer.
+        // For this we created the "Objects" layer, and assigned the objects on top of the plank to it.
+        int layerMask = LayerMask.GetMask("Objects");
+
+        Collider[] colliders = Physics.OverlapBox(
             position,
             halfExtents,
             transform.rotation,
@@ -22,6 +31,8 @@ public class Woodplank : MonoBehaviour
 
         foreach (var other in colliders)
         {
+            // To perform the shake visual, we use a simple animation that changes de rotation of the object
+            // fast enough to seem like it is shaking
             other.GetComponent<Animator>().SetTrigger("Shake");
         }
     }
